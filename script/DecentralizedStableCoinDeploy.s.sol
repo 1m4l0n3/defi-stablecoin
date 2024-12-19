27f78acc89ead73d56610b38e573effbde3edeee
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-12-19 15:52:56 +0530

    [Johny] Implement deploy function for DSC Engine

--- script/DecentralizedStableCoinDeploy.s.sol
+++ script/DecentralizedStableCoinDeploy.s.sol
@@ -4,14 +4,28 @@ pragma solidity ^0.8.18;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+ import {HelperConfig} from "./HelperConfig.s.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
 
 contract DecentralizedStableCoinDeploy is Script {
     DecentralizedStableCoin private stableCoin;
+    DSCEngine private dscEngine;
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
     address private owner;
+
     function run() public returns(DecentralizedStableCoin){
+        HelperConfig helperConfig = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = helperConfig.activeNetworkConfig();
+
+        tokenAddresses = [weth,wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed,wbtcUsdPriceFeed];
         owner = vm.parseAddress(vm.envString("OWNER_ADDRESS"));
-        vm.startBroadcast();
+
+        vm.startBroadcast(deployerKey);
         stableCoin = new DecentralizedStableCoin(owner);
+        dscEngine = new DSCEngine(tokenAddresses,priceFeedAddresses,address(stableCoin));
+        stableCoin.transferOwnership(address(dscEngine));
         vm.stopBroadcast();
         return stableCoin;
     }
